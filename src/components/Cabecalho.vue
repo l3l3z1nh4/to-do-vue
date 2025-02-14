e3af98ccf10196910c77aec1b3e579b3c56564e4
Letícia <lleticiapessotti@gmail.com>
2025-02-13 21:16:06 -0300

    to-do (componentizado)

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,10 @@
+<script setup>
+    const props = defineProps(['tarefasPendentes'])
+</script>
+
+<template>
+    <header class="p-5 mb-4 bg-light rounded-3">
+        <h1>minhas tarefas</h1>
+        <p>{{ props.tarefasPendentes}} pendentes</p>
+    </header>
+</template>
